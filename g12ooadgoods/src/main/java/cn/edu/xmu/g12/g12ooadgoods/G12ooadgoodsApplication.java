f5a28238b1dc9f704fe763fc4c134bf98d6728f5
ZhouShuren <61644268+ZhouShuren1881@users.noreply.github.com>
2020-12-14 16:51:40 +0800

    v4.0.0 Init Main Project
    初始化主项目-商品模块

---
+++ g12ooadgoods/src/main/java/cn/edu/xmu/g12/g12ooadgoods/G12ooadgoodsApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.xmu.g12.g12ooadgoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G12ooadgoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G12ooadgoodsApplication.class, args);
+    }
+
+}
---
+++ g12ooadgoods/src/test/java/cn/edu/xmu/g12/g12ooadgoods/G12ooadgoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.xmu.g12.g12ooadgoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G12ooadgoodsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
