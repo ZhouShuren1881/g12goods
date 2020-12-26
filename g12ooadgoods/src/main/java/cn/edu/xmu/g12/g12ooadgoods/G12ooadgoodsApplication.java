6bc690e828e40ce9e04421ee8c36728fcaaffbef
ZhouShuren <61644268+ZhouShuren1881@users.noreply.github.com>
2020-12-27 07:52:37 +0800

    116

--- g12ooadgoods/src/main/java/cn/edu/xmu/g12/g12ooadgoods/G12ooadgoodsApplication.java
+++ g12ooadgoods/src/main/java/cn/edu/xmu/g12/g12ooadgoods/G12ooadgoodsApplication.java
@@ -1,6 +1,5 @@
 package cn.edu.xmu.g12.g12ooadgoods;
 
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
