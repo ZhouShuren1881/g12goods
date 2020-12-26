6f57323861040249ac2eb2ee34c4066829384e9d
ZhouShuren <61644268+ZhouShuren1881@users.noreply.github.com>
2020-12-26 20:01:09 +0800

    update

--- oomall/other/src/main/java/cn/edu/xmu/oomall/other/model/Order.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.xmu.oomall.other.model;
-
-import lombok.Data;
-
-@Data
-public class Order {
-    private Long id;
-    private String name;
-}
--- order/sql/database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS oomall_log DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
-
-CREATE USER 'dbuser'@'localhost' IDENTIFIED BY '123456';
-CREATE USER 'dbuser'@'%' IDENTIFIED BY '123456';
-
-GRANT ALL ON oomall_log.* TO 'dbuser'@'localhost';
-GRANT ALL ON oomall_log.* TO 'dbuser'@'%';
-
-FLUSH PRIVILEGES;
