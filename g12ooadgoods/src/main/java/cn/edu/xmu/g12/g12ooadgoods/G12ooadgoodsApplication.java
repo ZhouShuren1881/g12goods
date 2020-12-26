26529afc43a1b488b41bef64702d0c1c6fbb3956
ZhouShuren <61644268+ZhouShuren1881@users.noreply.github.com>
2020-12-26 21:55:43 +0800

    Update G12ooadgoodsApplication.java

--- g12ooadgoods/src/main/java/cn/edu/xmu/g12/g12ooadgoods/G12ooadgoodsApplication.java
+++ g12ooadgoods/src/main/java/cn/edu/xmu/g12/g12ooadgoods/G12ooadgoodsApplication.java
@@ -1,11 +1,13 @@
 package cn.edu.xmu.g12.g12ooadgoods;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("cn.edu.xmu.g12.g12ooadgoods.mapper")
+@NacosPropertySource(dataId = "g12nacos-data-id", autoRefreshed = true)
 public class G12ooadgoodsApplication {
 
     public static void main(String[] args) {
