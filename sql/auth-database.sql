504c6c3982f340db3548d692f6b1fb9662fcaef3
ZhouShuren <61644268+ZhouShuren1881@users.noreply.github.com>
2020-12-27 10:46:56 +0800

    123

--- sql/auth-database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS oomall_auth DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
-
-CREATE USER 'dbuser'@'localhost' IDENTIFIED BY '123456';
-CREATE USER 'dbuser'@'%' IDENTIFIED BY '123456';
-
-GRANT ALL ON oomall_auth.* TO 'dbuser'@'localhost';
-GRANT ALL ON oomall_auth.* TO 'dbuser'@'%';
-
-FLUSH PRIVILEGES;
