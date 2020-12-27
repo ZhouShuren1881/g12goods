14f3f1084300b14ff3270f1dc8e0542af5ca1de5
ZhouShuren <61644268+ZhouShuren1881@users.noreply.github.com>
2020-12-27 20:31:12 +0800

    131

--- g12ooadgoods/src/main/java/cn/edu/xmu/g12/g12ooadgoods/config/HeaderConfig.java
+++ g12ooadgoods/src/main/java/cn/edu/xmu/g12/g12ooadgoods/config/HeaderConfig.java
@@ -1,6 +1,9 @@
 package cn.edu.xmu.g12.g12ooadgoods.config;
 
 
+import cn.edu.xmu.g12.g12ooadgoods.controller.CouponController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,11 +15,13 @@ import java.io.IOException;
 
 @Component
 public class HeaderConfig extends OncePerRequestFilter {
+    private static final Logger logger = LoggerFactory.getLogger(HeaderConfig.class);
 
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
                                     FilterChain filterChain) throws ServletException, IOException {
-        httpServletResponse.setContentType("application/json;charset=UTF-x8");
+        logger.info(httpServletRequest.getRequestURL().toString());
+        httpServletResponse.setContentType("application/json;charset=UTF-8");
         filterChain.doFilter(httpServletRequest, httpServletResponse);
     }
 }
