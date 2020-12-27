200c6e99cb6056b9ee191569d95f7056ba11a72b
ZhouShuren <61644268+ZhouShuren1881@users.noreply.github.com>
2020-12-27 08:02:22 +0800

    117

--- g12ooadgoods/src/main/java/cn/edu/xmu/g12/g12ooadgoods/G12ooadgoodsApplication.java
+++ g12ooadgoods/src/main/java/cn/edu/xmu/g12/g12ooadgoods/G12ooadgoodsApplication.java
@@ -1,11 +1,17 @@
 package cn.edu.xmu.g12.g12ooadgoods;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableDubbo(scanBasePackages = "cn.edu.xmu.order.service")
 @SpringBootApplication
 @MapperScan("cn.edu.xmu.g12.g12ooadgoods.mapper")
+@EnableDiscoveryClient
+@EnableScheduling
 public class G12ooadgoodsApplication {
 
     public static void main(String[] args) {
