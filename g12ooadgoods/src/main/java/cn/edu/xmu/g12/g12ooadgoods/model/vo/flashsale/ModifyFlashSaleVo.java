fafb5e9750ac6a2c80be3f3327c140969e7fb0a1
ZhouShuren <61644268+ZhouShuren1881@users.noreply.github.com>
2020-12-29 02:39:54 +0800

    156

--- g12ooadgoods/src/main/java/cn/edu/xmu/g12/g12ooadgoods/model/vo/flashsale/ModifyFlashSaleVo.java
+++ g12ooadgoods/src/main/java/cn/edu/xmu/g12/g12ooadgoods/model/vo/flashsale/ModifyFlashSaleVo.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @Data
 public class ModifyFlashSaleVo {
     @NotNull(message = "flashDate不得为空")
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+//    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime flashDate;
 }
--- g12ooadgoods/src/main/java/cn/edu/xmu/g12/g12ooadgoods/model/vo/flashsale/NewFlashSaleVo.java
+++ g12ooadgoods/src/main/java/cn/edu/xmu/g12/g12ooadgoods/model/vo/flashsale/NewFlashSaleVo.java
@@ -16,6 +16,7 @@ import java.time.LocalDateTime;
 @ApiModel("新FlashSale传值对象")
 public class NewFlashSaleVo {
     @NotNull(message = "flashDate 不得为空")
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+//    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime flashDate;
 }
